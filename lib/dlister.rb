50258cb181c7ed826992b03a70e58d0dd583596d
Anthony Cook <anthonymichaelcook@gmail.com>
2013-01-14 03:20:49 -0800

    Moved code from executable into module.

--- lib/dlister.rb
+++ lib/dlister.rb
@@ -4,8 +4,29 @@ require 'dlister/dir_list'
 module Dlister
   module_function
 
-  def list path
-    DirList.new path
+  def list paths
+    @paths = paths
+
+    @paths.sort.each_with_index do |path, index|
+      puts "#{path}:" if many?
+
+      puts DirList.new path
+
+      puts if many? && not_last?(index)
+    end
   end
+
+  def count
+    @paths.length
+  end
+
+  def not_last? index
+    index + 1 < count
+  end
+
+  def many?
+    count > 1
+  end
+
 end
 
